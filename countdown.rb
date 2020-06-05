55b3350fb760e2810da11ce56d56b6ce0e10f7aa
langan.kg Langan <langan.kg@gmail.com>
2020-06-05 12:21:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,19 @@
 #write your code here
 
 def countdown(x)
-  x = 11
-  while x > 1
+  while x > 0
     x -= 1
     puts "#{x} SECOND(S)!"
 
   end
 end
 
+return "HAPPY NEW YEAR!"
+
 def countdown_with_sleep(x)
-  sleep(5)
-  while x > 0
+  sleep(1)
     x -= 1
-    sleep(1)
-  end
+    end
 end
 
-return "HAPPY NEW YEAR!"
+
