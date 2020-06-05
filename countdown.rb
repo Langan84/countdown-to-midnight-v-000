2186185e4630d1e49f7adf23de9c5917e738c90a
langan.kg Langan <langan.kg@gmail.com>
2020-06-05 11:49:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,12 @@ def countdown(counter)
   while counter < 0
     counter -= 1
     puts "#{counter} SECOND(S)!"
-  
+    time = Time.now
     sleep 1.second
 
   end
 end
-puts "HAPPY NEW YEAR!"
+"HAPPY NEW YEAR!"
 
 #> time = Time.now
 #> sleep 2.seconds until Time.now > time + 10.seconds
